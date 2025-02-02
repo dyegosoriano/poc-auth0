6a36e7bf602bd798b04c8536b38f69c3b8162ef7
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-02 15:54:59 -0300

    feat: criação da rota de edição de permissões do usuários

--- back/src/routes/users.routes.ts
+++ back/src/routes/users.routes.ts
@@ -30,5 +30,27 @@ usersRoutes
       res.status(500).json({ error: 'Failed to list user roles' })
     }
   })
+  .post('/users/:user_id/roles', jwtCheck, async (req: Request, res: Response) => {
+    try {
+      const { user_id } = req.params
+      const { role_id } = req.body
+
+      if (!role_id) return res.status(400).json({ error: 'Role ID is required' })
+
+      const response = await management.users.getRoles({ id: user_id })
+
+      if (response.data.length > 0) {
+        const roles = response.data.map(item => item.id)
+        await management.users.deleteRoles({ id: user_id }, { roles })
+      }
+
+      await management.users.assignRoles({ id: user_id }, { roles: [role_id] })
+
+      res.status(200).json({ message: 'User role updated successfully' })
+    } catch (error) {
+      console.error('Error updating user role:', error)
+      res.status(500).json({ error: 'Failed to update user role' })
+    }
+  })
 
 export { usersRoutes }
