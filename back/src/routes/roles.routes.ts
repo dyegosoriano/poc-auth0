580d955c743f7896e33e33663c6993cd47c24386
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-02 14:14:13 -0300

    ajustes nas rota

--- back/src/routes/roles.routes.ts
+++ back/src/routes/roles.routes.ts
@@ -1,13 +1,16 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 
 import { jwtCheck, management } from '../middlewares/auth'
 
 const rolesRoutes = Router()
 
-rolesRoutes.get('/roles', jwtCheck, async (req: Request, res: Response) => {
-  const response = await management.roles.getAll()
-
-  return res.json({ roles: response.data })
+rolesRoutes.get('/roles', jwtCheck, async (_req: Request, res: Response) => {
+  try {
+    const response = await management.roles.getAll()
+    return res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to list roles' })
+  }
 })
 
 export { rolesRoutes }
--- back/src/routes/users.routes.ts
+++ back/src/routes/users.routes.ts
@@ -1,13 +1,25 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 
 import { jwtCheck, management } from '../middlewares/auth'
 
 const usersRoutes = Router()
 
-usersRoutes.get('/users', jwtCheck, async (req: Request, res: Response) => {
-  const response = await management.users.getAll()
-
-  return res.json({ users: response.data })
-})
+usersRoutes
+  .get('/users', jwtCheck, async (req: Request, res: Response) => {
+    try {
+      const response = await management.users.getAll()
+      return res.json(response.data)
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to list users' })
+    }
+  })
+  .get('/users/:id', jwtCheck, async (req: Request, res: Response) => {
+    try {
+      const response = await management.users.get({ id: req.params.id })
+      return res.json(response.data)
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to list users' })
+    }
+  })
 
 export { usersRoutes }
